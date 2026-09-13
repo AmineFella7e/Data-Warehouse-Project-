79ca9140ae1e77ff2f54ee2deb8b23a8f1378081
Amine Fella7e <aminefella7e@gmail.com>
2026-09-13 14:49:09 +0100

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -157,7 +157,7 @@ BEGIN
                 CASE 
                     WHEN UPPER(TRIM(gen)) IN ('F','FEMALE') THEN 'Female'
                     WHEN UPPER(TRIM(gen)) IN ('M','MALE') THEN 'Male'
-                    ELSE NULL
+                    ELSE 'n/a'
                 END AS gen                                                    
               FROM [bronze].[erp_cust_az12]; 
 
